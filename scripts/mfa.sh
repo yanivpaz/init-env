7f2dffada0495d3cd55222dd2039449ae3789460
yaniv paz <yanivpaz1973@gmail.com>
2021-08-18 17:45:40 +0300

    new changes

--- scripts/mfa.sh
+++ scripts/mfa.sh
@@ -5,7 +5,7 @@ MFA_SERIAL=$1
 # the MFA token
 MFA_TOKEN=$2
 
-aws sts get-session-token --serial-number  $MFA_SERIAL  --token-code $MFA_TOKEN >myfile
+aws sts get-session-token --serial-number  $MFA_SERIAL  --token-code $MFA_TOKEN  --region us-east-1 >myfile
 export AWS_ACCESS_KEY_ID=$(cat myfile | jq .Credentials.AccessKeyId |tr -d '"')
 export AWS_SECRET_ACCESS_KEY=$(cat myfile | jq .Credentials.SecretAccessKey  |tr -d '"')
 export AWS_SESSION_TOKEN=$(cat myfile | jq .Credentials.SessionToken  |tr -d '"' )
